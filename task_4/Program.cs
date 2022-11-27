ec7409cff64c1760666e6a743adeecf6f32869a7
lagunova1204 <lagunova.irina1204@gmail.com>
2022-11-27 19:08:39 +0300

    Задача 4

---
+++ task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите три числа");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+if (A>B && A>C)
+{
+    Console.WriteLine(" MAX " +A);
+}
+else if (B>A && B>C)
+{
+     Console.WriteLine(" MAX " +B);
+}
+else
+{
+    Console.WriteLine(" MAX " +C);
+}
