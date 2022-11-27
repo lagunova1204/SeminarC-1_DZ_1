e220869a308c56347279a38df0beb6ba8023d16d
lagunova1204 <lagunova.irina1204@gmail.com>
2022-11-27 18:59:33 +0300

    Задача 2

---
+++ task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 2 числа");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+    Console.WriteLine(" Max "+ numberA);
+    Console.WriteLine(" Min "+ numberB);
+}
+else
+{
+    Console.WriteLine(" Max "+numberB);
+    Console.WriteLine(" Min "+numberA); 
+}
