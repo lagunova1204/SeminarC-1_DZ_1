0c670e278e78418b0dbb1ad6ab062180327af64f
lagunova1204 <lagunova.irina1204@gmail.com>
2022-11-27 19:32:41 +0300

    Задача 8

---
+++ task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index <= N)
+{
+    if (index % 2 == 0)
+    {
+       Console.Write(index +", ");
+       index++;
+    }
+    else
+    {
+        index++;
+    }
+}
